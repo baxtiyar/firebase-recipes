4d9d1ea8c6cfc4b07fa239e1b2c41a9dea93351e
baxtiyar <mem_baxtiyar@yahoo.com>
2023-05-06 16:23:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import logo from "./logo.svg";
+import "./App.css";
+// eslint-disable-next-line no-unused-vars
+import firebase from "./FirebaseConfig";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes----!!!!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
